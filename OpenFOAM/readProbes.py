193c22724b9a6752a2fb6b2fb4376d1c134cea00
simwes <36754185+simwes@users.noreply.github.com>
2019-09-25 13:44:40 -0700

    Update readProbes.py

--- OpenFOAM/readProbes.py
+++ OpenFOAM/readProbes.py
@@ -1,3 +1,9 @@
+#==================================================================================
+# This loads probes values of the three components of U 
+# and store then in a 3D matrix, e.g. (time, U component, probe) .
+# The input parameters are the name of the field 'U' and the number of probes.
+#===============================================================================
+
 import matplotlib, pprint
 import matplotlib.pyplot as plt
 import numpy as np
